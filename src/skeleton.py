7883769663440bdd737c97965a08fc921641e751
christochow <christochow@yahoo.com.hk>
2018-05-24 16:57:47 -0400

    allows user to input target directory via command line

--- src/skeleton.py
+++ src/skeleton.py
@@ -13,7 +13,7 @@ directory = os.fsencode(sys.argv[1])
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".txt"): 
-        df = pd.read_csv("C:/Users/chris/Downloads"+"/"+filename,sep='\t')
+        df = pd.read_csv(sys.argv[1]+"/"+filename,sep='\t')
         print(df[df['RIGHT_IN_BLINK']==1].head())
         break;
     else:
