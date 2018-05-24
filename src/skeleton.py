7a55fd5ee12952c0ab2ac4bf25a2231ad99fac47
christochow <christochow@yahoo.com.hk>
2018-05-24 16:55:55 -0400

    first commit

---
+++ src/skeleton.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import configparser as CP
+import sys, os
+
+def read_config():
+    Config = CP.ConfigParser()
+    Config.read("options.ini")
+    option = Config['DEFAULT']
+    return option
+
+
+directory = os.fsencode(sys.argv[1])
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".txt"): 
+        df = pd.read_csv("C:/Users/chris/Downloads"+"/"+filename,sep='\t')
+        print(df[df['RIGHT_IN_BLINK']==1].head())
+        break;
+    else:
+        continue
+op = read_config()
+print(list(op))
