d3fd6100adf6c8b482969babbf01e6603218fa9c
Hyun Jin Choi <calvinhyun.choi@mail.utoronto.ca>
2018-05-29 15:41:48 -0400

    Added timing, more console output.

--- src/skeleton.py
+++ src/skeleton.py
@@ -2,6 +2,7 @@ import pandas as pd
 import configparser as cp
 import os
 import argparse
+from datetime import datetime
 
 
 def read_config():
@@ -18,8 +19,10 @@ def parse_args():
     return parser.parse_args()
 
 
-def manipulate_df(df_old):
-    df_new = df_old[df_old['RIGHT_IN_BLINK'] == 1].head(1)
+def manipulate_df(df_old, filename):
+    blink_count = len(df_old['RIGHT_IN_BLINK'] == 1)
+    df_new = df_old[df_old['RIGHT_IN_BLINK'] == 0]
+    print('\tRemoved {} blink(s)'.format(blink_count))
     return df_new
 
 
@@ -28,17 +31,24 @@ if __name__ == '__main__':
 
     resampled_count = 0
 
+    op_start_time = datetime.now()
     directory = os.fsencode(args.dir)
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         if filename.endswith(".txt") and not filename.endswith("_processed.txt"):
+            file_start_time = datetime.now()
+
+            print('Working on {}:'.format(filename))
+            print('\tReading...'.format(filename))
             df = pd.read_csv(os.path.join(args.dir, filename), sep='\t')
-            df = manipulate_df(df)
+            df = manipulate_df(df, filename)
 
             new_name = os.path.join(args.dir, filename.replace(".txt", "_processed.txt"))
+            print('\tExporting...'.format(filename))
             if os.path.isfile(new_name):
-                print('WARNING: Overriding {}'.format(new_name))
+                print('\t\tWARNING: Overriding {}'.format(os.path.abspath(new_name)))
             df.to_csv(new_name, sep='\t')
+            print('\tExport successful! ({})'.format(datetime.now() - file_start_time))
             resampled_count += 1
 
-    print('Operation success: resampled {} dataframe(s)'.format(resampled_count))
+    print('Operation success: resampled {} dataframe(s) ({})'.format(resampled_count, datetime.now() - op_start_time))
