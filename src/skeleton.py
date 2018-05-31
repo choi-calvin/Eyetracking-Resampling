1efd6f688f67a4a362150646163d2015f88aabae
Hyun Jin Choi <calvinhyun.choi@mail.utoronto.ca>
2018-05-31 15:10:14 -0400

    Added customizable file extension support.

--- src/skeleton.py
+++ src/skeleton.py
@@ -4,6 +4,10 @@ import os
 import argparse
 from datetime import datetime
 
+PERCENT_PREC = 3
+TIME_PREC = 5
+FILE_TYPES = ('.txt', '.text')
+
 
 def read_config():
     config = cp.ConfigParser()
@@ -20,9 +24,10 @@ def parse_args():
 
 
 def manipulate_df(df_old):
-    blink_count = len(df_old['RIGHT_IN_BLINK'] == 1)
+    blink_count = len(df_old[df_old['RIGHT_IN_BLINK'] == 1])
     df_new = df_old[df_old['RIGHT_IN_BLINK'] == 0]
-    print('\tRemoved {} blink(s)'.format(blink_count))
+    print('\tRemoved {} blink(s) ({:.{prec}f}% of total)'.format(blink_count, blink_count / len(df_old) * 100,
+                                                                 prec=PERCENT_PREC))
     return df_new
 
 
@@ -35,7 +40,8 @@ if __name__ == '__main__':
     directory = os.fsencode(args.dir)
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
-        if filename.endswith(".txt") and not filename.endswith("_processed.txt"):
+        filesplit = os.path.splitext(filename)
+        if filename.endswith(FILE_TYPES) and not filesplit[0].endswith("_processed"):
             file_start_time = datetime.now()
 
             print('Working on {}:'.format(filename))
@@ -43,14 +49,14 @@ if __name__ == '__main__':
             df = pd.read_csv(os.path.join(args.dir, filename), sep='\t')
             df = manipulate_df(df)
 
-            new_name = os.path.join(args.dir, filename.replace(".txt", "_processed.txt"))
+            new_name = os.path.join(args.dir, filesplit[0] + "_processed" + filesplit[1])
             print('\tExporting...'.format(filename))
             if os.path.isfile(new_name):
                 print('\t\tWARNING: Overriding {}'.format(os.path.abspath(new_name)))
             df.to_csv(new_name, sep='\t')
             print('\tExport successful!')
-            print('\tTotal {:5f}s'.format((datetime.now() - file_start_time).total_seconds()))
+            print('\tTotal {:{prec}f}s'.format((datetime.now() - file_start_time).total_seconds(), prec=TIME_PREC))
             resampled_count += 1
 
-    print('Operation success: resampled {} dataframe(s) (total {:.5f}s)'.format(resampled_count, (
-            datetime.now() - op_start_time).total_seconds()))
+    print('Operation success: resampled {} dataframe(s) (total {:.{prec}f}s)'.format(resampled_count, (
+            datetime.now() - op_start_time).total_seconds(), prec=TIME_PREC))
