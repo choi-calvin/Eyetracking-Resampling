72f0a7cc63c50b5769d839b3b62985bf00d5616e
Hyun Jin Choi <calvinhyun.choi@mail.utoronto.ca>
2018-06-08 18:58:45 -0400

    Styling console output.

--- src/script.py
+++ src/script.py
@@ -18,8 +18,10 @@ import argparse
 from argparse import RawDescriptionHelpFormatter
 from datetime import datetime
 
-# Constants, can be overwritten by config file or script arguments
-FILE_TYPES = ('.txt', '.text')
+# Constants, can be overwritten by config file
+CONFIG_FILE = 'options.ini'
+
+FILE_TYPES = ('txt', 'text')
 RESAMPLING_RATE = 5
 GROUP_BY = 'TRIAL_INDEX'
 
@@ -35,11 +37,11 @@ def str_to_int(str_to_convert, default):
         converted_str = int(str_to_convert)
         return converted_str
     except ValueError as e:
-        print("ERROR: {} in config file is not a valid integer, using default".format(e))
+        print("\tERROR: {} is not a valid integer, using default {}".format(str_to_convert, default))
     return default
 
 
-# Overwrite constants from config file, if the exist
+# Overwrite constants from config file, if they exist
 def read_config():
     global FILE_TYPES
     global RESAMPLING_RATE
@@ -50,11 +52,11 @@ def read_config():
 
     config = cp.ConfigParser()
     config.optionxform = str
-    config.read('options.ini')  # By default, config file is named 'options.ini' in the same folder as script
+    config.read(CONFIG_FILE)  # By default, config file is named 'options.ini' in the same folder as script
 
     if 'SETTINGS' in config:
         if 'FILE_TYPES' in config['SETTINGS']:
-            FILE_TYPES = tuple(config['SETTINGS']['FILE_TYPES'].split(', '))
+            FILE_TYPES = tuple(config['SETTINGS']['FILE_TYPES'].split(','))
         if 'RESAMPLING_RATE' in config['SETTINGS']:
             RESAMPLING_RATE = str_to_int(config['SETTINGS']['RESAMPLING_RATE'], RESAMPLING_RATE)
         if 'GROUP_BY' in config['SETTINGS']:
@@ -62,12 +64,12 @@ def read_config():
 
     if 'AGGREGATE TYPE' in config:
         for variable in config['AGGREGATE TYPE']:
-            agg_types = config['AGGREGATE TYPE'][variable].split(', ')
+            agg_types = config['AGGREGATE TYPE'][variable].split(',')
 
             for agg_type in agg_types:
                 if agg_type not in COMMON_AGGREGATE_TYPES:
                     print(
-                        "WARNING: {} in config file is not a common aggregate type, may cause crashes".format(agg_type))
+                        "\tWARNING: {} is not a common aggregate type, may cause crashes".format(agg_type))
                 if variable in AGGREGATIONS:
                     AGGREGATIONS[variable].append(agg_type)
                 else:
@@ -86,8 +88,9 @@ def parse_args():
         epilog="""
         ***
         Resamples all specified file types in the specified directory and outputs in new file with suffix _processed.
-        Column variables and their aggregate types are specified in options.ini, but can be overwritten by arguments
-        running the script. If column variables do not exist in a dataset, they are skipped.
+        Column variables and their aggregate types are specified in options.ini. If column variables do not exist in a
+        dataset, they are skipped.
+        
         Designed for use in Duncan Lab with eyetracking experiment data.
         ***
         """,
@@ -101,23 +104,23 @@ def remove_blinks(df_old):
     print("\tRemoving blinks...", end='')
     blink_count = len(df_old[df_old['RIGHT_IN_BLINK'] == 1])
     df_new = df_old[df_old['RIGHT_IN_BLINK'] == 0]
-    print("successful!\n\t\tRemoved {} blink(s) ({:.{prec}f}% of total)".format(blink_count,
-                                                                                blink_count / len(df_old) * 100,
-                                                                                prec=PERCENT_PREC))
+    print("success!\n\t\tRemoved {} blink(s) ({:.{prec}f}% of total)".format(blink_count,
+                                                                             blink_count / len(df_old) * 100,
+                                                                             prec=PERCENT_PREC))
     return df_new
 
 
 def bin_df(df_old):
     if GROUP_BY not in df_old.columns:
-        print("ERROR: {} not found in dataset, exiting...".format(GROUP_BY))
-        exit(1)
+        print("\tERROR: {} not found in dataset, skipping...".format(GROUP_BY))
+        return None
     # Groups every RESAMPLING_RATE rows, and by GROUP_BY to ensure no grouping across trials
     print("\tGrouping every {} rows...".format(RESAMPLING_RATE), end='')
     df_groupby = df_old.groupby([GROUP_BY, (df_old.index // RESAMPLING_RATE) * RESAMPLING_RATE], as_index=False)
-    print("succesful!")
+    print("success!")
     print("\tComputing and aggregating data...", end='')
     df_new = aggregate(df_groupby, df_old)
-    print("successful!")
+    print("success!")
     df_new = df_new.set_index(GROUP_BY)
     return df_new
 
@@ -130,35 +133,46 @@ def aggregate(df_groupby, df):
 
 
 def main():
+    print("Reading from config file '{}':".format(CONFIG_FILE))
     read_config()
+    print("\tSuccess!")
+    print("Parsing arguments:")
     args = parse_args()
+    print("\tSuccess!")
 
     resampled_count = 0
+    error_count = 0
 
-    op_start_time = datetime.now()
+    op_start_time = datetime.now()  # For the calculation of total operation length
     directory = os.fsencode(args.dir)
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         filesplit = os.path.splitext(filename)
         if filename.endswith(FILE_TYPES) and not filesplit[0].endswith('_processed'):
-            file_start_time = datetime.now()
+            file_start_time = datetime.now()  # For current operation length
 
             print("Working on {}:".format(filename))
             print("\tReading...".format(filename), end='')
             df = pd.read_csv(os.path.join(args.dir, filename), sep='\t', na_values='.')
-            print("successful!")
+            print("success!")
+
             df = remove_blinks(df)
             df = bin_df(df)
+            if df is None:
+                error_count += 1
+                resampled_count = 0 if (resampled_count == 0) else (resampled_count - 1)
+                continue
 
             new_name = os.path.join(args.dir, filesplit[0] + '_processed' + filesplit[1])
             print("\tExporting...".format(filename), end='')
             df.to_csv(new_name, sep='\t')
-            print("successful!")
+            print("success!")
             if os.path.isfile(new_name):
                 print("\t\tWARNING: Overrode {}".format(os.path.abspath(new_name)))
             print("\tTotal {:{prec}f}s".format((datetime.now() - file_start_time).total_seconds(), prec=TIME_PREC))
             resampled_count += 1
 
+    print("\n{} dataframe(s) could not be resampled".format(error_count))
     print("Operation success: resampled {} dataframe(s) (total {:.{prec}f}s)".format(resampled_count, (
             datetime.now() - op_start_time).total_seconds(), prec=TIME_PREC))
 
